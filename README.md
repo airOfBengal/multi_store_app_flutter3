4d990a3e6772955908a214601081504fef3906c6
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2022-09-08 13:05:10 +0600

    Initial commit - Adds bottom navigation items.

---
+++ README.md
@@ -0,0 +1,9 @@
+# multi_store_app
+
+A new Flutter project.
+
+## Getting Started
+
+Building E-Commerce Multi Shop App from scratch with 
+Flutter 3, Firebase and Stripe payment Gateway.
+
---
+++ lib/main_screens/customer_home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CustomerHomeScreen extends StatefulWidget {
+  const CustomerHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CustomerHomeScreen> createState() => _CustomerHomeScreenState();
+}
+
+class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Category',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: 'Stores',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
